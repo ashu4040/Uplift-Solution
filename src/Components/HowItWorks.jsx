6a1861c1880fa9871eab038d14f6859da25bfd25
Ayush Kumar <ak433778@gmail.com>
2025-04-28 17:48:58 +0530

    updates

--- src/Components/HowItWorks.jsx
+++ src/Components/HowItWorks.jsx
@@ -41,7 +41,7 @@ const HowItWorks = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-8 md:px-14">
         <div className="flex flex-col md:flex-row items-center justify-center gap-8 mb-12">
           <div className="w-full md:w-[40%] text-center">
-            <h3 className="text-3xl sm:text-4xl font-bold mb-4 overflow-hidden p-1">
+            <h3 className="text-lg sm:text-3xl font-bold mb-4 overflow-hidden p-1">
               {data?.Step_1?.Title}
             </h3>
             <p className="text-lg p-1">{data?.Step_1?.Description}</p>
@@ -57,7 +57,7 @@ const HowItWorks = () => {
 
         <div className="flex flex-col md:flex-row-reverse items-center justify-center gap-8 mb-12">
           <div className="w-full md:w-[40%] text-center">
-            <h3 className="text-3xl sm:text-4xl font-bold mb-4 overflow-hidden p-1">
+            <h3 className="text-lg sm:text-3xl font-bold mb-4 overflow-hidden p-1">
               {data?.Step_2?.Title}
             </h3>
             <p className="text-lg p-1">{data?.Step_2?.Description}</p>
@@ -73,7 +73,7 @@ const HowItWorks = () => {
 
         <div className="flex flex-col md:flex-row items-center justify-center gap-8 mb-12">
           <div className="w-full md:w-[40%] text-center">
-            <h3 className="text-3xl sm:text-4xl font-bold mb-4 overflow-hidden p-1">
+            <h3 className="text-lg sm:text-3xl font-bold mb-4 overflow-hidden p-1">
               {data?.Step_3?.Title}
             </h3>
             <p className="text-lg p-1">{data?.Step_3?.Description}</p>
@@ -89,7 +89,7 @@ const HowItWorks = () => {
 
         <div className="flex flex-col md:flex-row-reverse items-center justify-center gap-8 mb-12">
           <div className="w-full md:w-[40%] text-center">
-            <h3 className="text-3xl sm:text-4xl font-bold mb-4 overflow-hidden p-1">
+            <h3 className="text-lg sm:text-3xl font-bold mb-4 overflow-hidden p-1">
               {data?.Step_4?.Title}
             </h3>
             <p className="text-lg p-1">{data?.Step_4?.Description}</p>
